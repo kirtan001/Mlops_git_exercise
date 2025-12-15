d938b749468e537bd0d90184fd07aa1f225d8ea4
kirtan001 <kirtanadeshara1710@gmail.com>
2025-12-15 17:17:40 +0530

    addinf helloworld.py to github

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+class HelloWorld:
+    @staticmethod
+    def main() -> None:
+        print("Hello, Class!")
+        
+
+
+def main() -> None:
+    HelloWorld.main()
+
+
+if __name__ == "__main__":
+    main()
