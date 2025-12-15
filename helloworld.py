d75ec4931833287fe234b56102270c9b1f39f9cc
kirtan001 <kirtanadeshara1710@gmail.com>
2025-12-15 17:50:43 +0530

    adding files

--- helloworld.py
+++ helloworld.py
@@ -2,12 +2,10 @@ class HelloWorld:
     def main() -> None:
         print("Hello, Class!")
         print("Hello India")
+        print("Hello MSC Data Science")
         
-
-
 def main() -> None:
     HelloWorld.main()
-
 
 if __name__ == "__main__":
     main()
